6d57ac08983a3225e930441a71cfaa5aebac8647
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-05-01 10:54:56 +0500

    show on web page

---
+++ text editor/app.js
@@ -0,0 +1,36 @@
+// var textarea = document.getElementById("text");
+// var flag = true;
+
+// function bold() {
+//   if (flag === true) {
+//     textarea.style.fontWeight = "900";
+//     flag = false;
+//   } else {
+//     textarea.style.fontWeight = "100";
+//     flag = true;
+//   }
+// }
+// //italic
+// function italic() {
+//   if (flag === true) {
+//     textarea.style.textDecoration = "italic";
+
+//   }
+// }
+// function todo() {
+//   var todo = document.getElementById("text").value;
+//   var usertext = document.getElementById("usertext");
+//   if (todo.length === 0) {
+//     usertext.innerHTML = "plz add text";
+//   } else {
+//     var todo = document.getElementById("text").value;
+//     usertext.innerHTML = "";
+//     usertext.style.display = "none";
+//     // window.localStorage.setItem("wikki", todo);
+//     var local = window.localStorage.getItem("wikki");
+//     localStorage.getItem(text, local);
+//   }
+// }
+function todo() {
+  var usertext = document.getElementById("text");
+}
